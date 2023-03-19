2829f08122c8cee76c933a0302d093f9b69e4d16
adityanegii <aditya.negi@mail.mcgill.ca>
2023-03-19 14:53:57 -0400

    pyinstaller update

--- files/constants.py
+++ files/constants.py
@@ -6,18 +6,15 @@ ROWS, COLS = 8, 8
 SQUARE_SIZE = WIDTH // COLS
 
 # COLORS
-WHITE_PIECES = (252, 242, 239)
-BLACK_PIECES = (49, 17, 8)
-LIGHT_SQUARES = (230, 195, 165)
-DARK_SQUARES = (181, 101, 29)
-DARK_SQUARES_MOVE = (200, 200, 36)
-LIGHT_SQUARES_MOVE = (255, 225, 0)
+WHITE_PIECES = (255, 255, 255)
+BLACK_PIECES = (0, 0, 0)
+LIGHT_SQUARES = (240, 217, 181)
+DARK_SQUARES = (139, 69, 19)
+DARK_SQUARES_MOVE = (255, 215, 0)
 BLACK = (0, 0, 0)
-BLUE = (0, 0, 255)
-GREY = (128, 128, 128)
+BLUE =  (0,0,255)
 
 # DEPTH for move search
-DEPTH = 2
 INF = float('inf')
 
 # Crown source: http://clipart-library.com/clipart/crown-clip-art-18.htm
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from files.constants import WIDTH, HEIGHT, BLUE, WHITE_PIECES, DEPTH, BLACK_PIECES, INF, GREY
+from files.constants import WIDTH, HEIGHT, BLUE, WHITE_PIECES, BLACK_PIECES, DARK_SQUARES
 from files.board import Board
 import time
 from files.search import minimax
@@ -11,6 +11,7 @@ pygame.display.set_caption('Checkers')
 
 pygame.font.init()
 font = pygame.font.SysFont('Comic Sans MS', 60)
+font2 = pygame.font.SysFont('Comic Sans MS', 24)
 
 
 def get_click_pos(pos):
@@ -24,7 +25,7 @@ def end_screen(board, win):
     '''Get the end screen when game is won'''
     board.winner()
     if board.victor != None:
-        textsurface = font.render(board.victor, False, BLUE)
+        textsurface = font.render(board.victor, True, BLACK_PIECES)
         size = textsurface.get_size()
         win.blit(textsurface, (400 - size[0] / 2, 400 - size[1] / 2))
         pygame.display.update()
@@ -33,24 +34,31 @@ def end_screen(board, win):
     return True
 
 
-def obtain_piece(win, board, piece):
+def obtain_piece(board, piece):
     '''Get a piece on the board and draw the board (to see the valid moves)'''
     board.selected_piece = piece
     board.valid_moves(board.selected_piece)
     board.draw(WIN)
 
 
-def get_depth(win):
+def get_depth():
+    WIN.fill(DARK_SQUARES)
+    textsurface1 = font2.render("Choose difficulty", True, WHITE_PIECES)
+    size1 = textsurface1.get_size()
+    WIN.blit(textsurface1, (400 - size1[0] / 2, 100))
+    # pygame.draw.rect(WIN, GREY, (200, 325, 400, 150), False)
+    pygame.draw.rect(WIN, WHITE_PIECES,  (190, 325, 7, 150))
+    pygame.draw.rect(WIN, WHITE_PIECES,  (190, 325, 400, 7))
+    pygame.draw.rect(WIN, WHITE_PIECES,  (190, 475, 407, 7))
+    for i in range(1, 5):
+        d = str(i)
+        textsurface = font.render(d, True, WHITE_PIECES)
+        size = textsurface.get_size()
+        WIN.blit(textsurface, (225 + 100*(i-1), 400 - size[1] / 2))
+        pygame.draw.rect(WIN, WHITE_PIECES,  (290+100*(i-1), 325, 7, 150))
+    
+    pygame.display.update()
     while True:
-        pygame.draw.rect(WIN, GREY, (200, 325, 400, 150), False)
-        pygame.draw.rect(WIN, BLUE,  (190, 325, 10, 150))
-        for i in range(1, 5):
-            d = str(i)
-            textsurface = font.render(d, False, BLUE)
-            size = textsurface.get_size()
-            WIN.blit(textsurface, (225 + 100*(i-1), 400 - size[1] / 2))
-            pygame.draw.rect(WIN, BLUE,  (290+100*(i-1), 325, 10, 150))
-        pygame.display.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 quit()
@@ -66,7 +74,23 @@ def get_depth(win):
                         return 3
                     elif x in range(500, 590):
                         return 4
-                
+
+def choose_players():
+    WIN.fill(DARK_SQUARES)
+    textsurface = font2.render("Press 1 to play against computer, 2 to play against human", True, WHITE_PIECES)
+    size = textsurface.get_size()
+    WIN.blit(textsurface, (400 - size[0] / 2, 100))
+    pygame.display.update()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_1:
+                    return 1
+                elif event.key == pygame.K_2:
+                    return 2 
+
 def main():
     '''Main program for running the game'''
     run = True
@@ -77,62 +101,98 @@ def main():
 
     # variable to store selected piece
     piece = None
+    players = choose_players()
+    if players == 1:
+        depth = get_depth()
 
-    depth = get_depth(WIN)
+    if players == 1:
+        while run:
+                
+            clock.tick(FPS)
 
-    while run:
-        clock.tick(FPS)
-
-        end_screen(board, WIN)
-        board.draw(WIN)
-        # If it is black's/computer's turn
-        if board.turn == BLACK_PIECES:
-            evaluation, new_board = minimax(board, depth, False)
-            board = new_board
+            end_screen(board, WIN)
             board.draw(WIN)
-            run = end_screen(board, WIN)
+            # If it is black's/computer's turn
+            if board.turn == BLACK_PIECES:
+                evaluation, new_board = minimax(board, depth, False)
+                board = new_board
+                board.draw(WIN)
+                run = end_screen(board, WIN)
 
-        # To have second player as ai uncomment the next section and comment out the for loop
-        # elif board.turn == WHITE_PIECES:
-        #     evaluation, new_board = minimax(board, depth, True)
-        #     board = new_board
-        #     board.draw(WIN)
-        #     end_screen(board, WIN)
+            # If it is white's/player's turn
+            for event in pygame.event.get():
 
-        # If it is white's/player's turn
-        for event in pygame.event.get():
+                # Quit the game
+                if event.type == pygame.QUIT:
+                    run = False
 
-            # Quit the game
-            if event.type == pygame.QUIT:
-                run = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    pos = event.pos
+                    row, col = get_click_pos(pos)
+                    piece = board.get_piece(row, col)
 
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                pos = event.pos
-                row, col = get_click_pos(pos)
-                piece = board.get_piece(row, col)
-
-                if board.in_chain != None:
-                    if row == board.in_chain.row and col == board.in_chain.col:
-                        obtain_piece(WIN, board, piece)
+                    if board.in_chain != None:
+                        if row == board.in_chain.row and col == board.in_chain.col:
+                            obtain_piece(board, piece)
+                        else:
+                            board.change_turn()
                     else:
-                        board.change_turn()
-                else:
-                    if piece == 0:
-                        board.selected_piece = None
-                    if piece != 0 and piece.color == board.turn:
-                        obtain_piece(WIN, board, piece)
+                        if piece == 0:
+                            board.selected_piece = None
+                        if piece != 0 and piece.color == board.turn:
+                            obtain_piece(board, piece)
 
-            if event.type == pygame.MOUSEBUTTONUP:
-                if board.selected_piece != None:
-                    n_pos = event.pos
-                    n_row, n_col = get_click_pos(n_pos)
-                    board.move(n_row, n_col)
-                    board.draw(WIN)
+                if event.type == pygame.MOUSEBUTTONUP:
+                    if board.selected_piece != None:
+                        n_pos = event.pos
+                        n_row, n_col = get_click_pos(n_pos)
+                        board.move(n_row, n_col)
+                        board.draw(WIN)
 
-        board.draw(WIN)
-        pygame.display.update()
+            board.draw(WIN)
+            pygame.display.update()
+    if players == 2:
+        while run:
+                
+            clock.tick(FPS)
+
+            end_screen(board, WIN)
+            board.draw(WIN)
+            
+            for event in pygame.event.get():
+
+                # Quit the game
+                if event.type == pygame.QUIT:
+                    run = False
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    pos = event.pos
+                    row, col = get_click_pos(pos)
+                    piece = board.get_piece(row, col)
+
+                    if board.in_chain != None:
+                        if row == board.in_chain.row and col == board.in_chain.col:
+                            obtain_piece(board, piece)
+                        else:
+                            board.change_turn()
+                    else:
+                        if piece == 0:
+                            board.selected_piece = None
+                        if piece != 0 and piece.color == board.turn:
+                            obtain_piece(board, piece)
+
+                if event.type == pygame.MOUSEBUTTONUP:
+                    if board.selected_piece != None:
+                        n_pos = event.pos
+                        n_row, n_col = get_click_pos(n_pos)
+                        board.move(n_row, n_col)
+                        board.draw(WIN)
+
+            board.draw(WIN)
+            pygame.display.update()
 
     pygame.quit()
 
 
-main()
+if __name__ == "__main__":
+    main()
